d1eb947db3f74b2afab2de7df81b5a261a5768c2
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-03-12 12:38:00 +0300

    add router for all methods

--- routes/wrong-request-router.js
+++ routes/wrong-request-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('*', (req, res) => {
+router.use('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
