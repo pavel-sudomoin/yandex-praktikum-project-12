32a1ae991b59b0e6c84969ae0835f2e42d61544f
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-03-26 18:31:46 +0300

    add CORS middleware

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  next();
 });
 
 app.use(bodyParser.json());
