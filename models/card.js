d83384e300b52a46ca6da8b8d1158256a4bf390f
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-04-07 15:32:42 +0300

    add error messages for card model

--- models/card.js
+++ models/card.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 
-const urlValidatorForMongoose = require('../validators/url-validator-for-mongoose');
+const urlValidator = require('../validators/url-validator');
 
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+    minlength: [2, 'Название карточки должно быть длиннее 2 символов'],
+    maxlength: [30, 'Название карточки должно быть короче 30 символов'],
+    required: [true, 'Не указано название карточки'],
   },
   link: {
     type: String,
-    validate: urlValidatorForMongoose,
-    required: true,
+    validate: [urlValidator, 'Некорректная ссылка на картинку'],
+    required: [true, 'Не указана ссылка на картинку'],
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
