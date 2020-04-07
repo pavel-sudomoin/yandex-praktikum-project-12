f81e514c5b8ac156ec86a1f6f66b279a3deee466
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-04-07 15:27:22 +0300

    add select: false for password

--- models/user.js
+++ models/user.js
@@ -31,12 +31,13 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     minlength: [8, 'Пароль должен быть не короче 8 символов'],
+    select: false,
     required: [true, 'Не указан пароль'],
   },
 });
 
 userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
         return Promise.reject(new Error('Неправильные почта или пароль'));
